bfe01c9c710c4896c143cf230d0a5dcce87703cf
Christian Fredricks <Christianpfred@gmail.com>
2021-08-02 16:23:55 -0500

    Added proper working code

--- server/routes/books.router.js
+++ server/routes/books.router.js
@@ -1,15 +1,19 @@
 const express = require ('express');
 
+// Setup an array to store my book objects
+const bookList = [];
+
 // Make a router
 // A router is like a mini-app
 const router = express.Router();
 
 // We can use a `router` just like we use app
-router.get('/book', (req,res) => {
+// the actual URL here is "/book"
+router.get('/', (req,res) => {
     res.send(bookList);
 });
 
-router.post('/book', (req, res) => {
+router.post('/', (req, res) => {
     bookList.push(req.body);
     res.sendStatus(200);
 });
